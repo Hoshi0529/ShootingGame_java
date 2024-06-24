0b2b440a32c0835e329f55da36ddb4e65ec8c7a7
星 優太 <youtaixing753@gmail.com>
2024-06-24 12:03:49 +0900

    13.4Enterキーが押されたらもう一度ゲームを実行する

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -3,28 +3,38 @@ public class GameFrame extends MyFrame{
 	public void run() {
 		GameWorld.player = new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
-		GameWorld.playerBullets=new Vector<PlayerBullet>();
-		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
-		GameWorld.enterPressed=false;
 		while(true) {
-			clear();
-			GameWorld.player.draw(this);
-			GameWorld.player.move();
-			movePlayerBullets();
-			moveEnemies();
-			checkPlayerAndEnemis();
-			checkPlayerBulletsAndEnemies();
-			if(GameWorld.enemies.size()==0) {
-				setColor(0,0,0);
-				drawString("クリア!",100,200,40);
-			}else if(GameWorld.player.y<0) {
-				setColor(0,0,0);
-				drawString("ゲームオーバー!",50,200,40);
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
+			GameWorld.playerBullets=new Vector<PlayerBullet>();
+			GameWorld.enemies=new Vector<Enemy>();
+			GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+			GameWorld.enterPressed=false;
+			while(true) {
+				clear();
+				GameWorld.player.draw(this);
+				GameWorld.player.move();
+				movePlayerBullets();
+				moveEnemies();
+				checkPlayerAndEnemis();
+				checkPlayerBulletsAndEnemies();
+				if(GameWorld.enemies.size()==0) {
+					setColor(0,0,0);
+					drawString("クリア!",100,200,40);
+					if(GameWorld.enterPressed) {
+						break;
+					}
+				}else if(GameWorld.player.y<0) {
+					setColor(0,0,0);
+					drawString("ゲームオーバー!",50,200,40);
+					if(GameWorld.enterPressed) {
+						break;
+					}
+				}
+				sleep(0.03);
 			}
-			sleep(0.03);
-		}
-	}
+	     }
+     }
 	public void moveEnemies() {
 		for(int i=0; i<GameWorld.enemies.size();i++) {
 			Enemy e=GameWorld.enemies.get(i);      
@@ -32,8 +42,8 @@ public class GameFrame extends MyFrame{
 			e.move();
 		}
 		int i=0;
-		while(i<GameWorld.enemies.size()) {
-			Enemy e=GameWorld.enemies.remove(i);
+		while (i<GameWorld.enemies.size()) {
+			Enemy e=GameWorld.enemies.get(i);
 			if(e.y>400) {
 				GameWorld.enemies.remove(i);
 			}else {
